4d3b7ab472ec876353d3842b71583681d21ecc3b
eugeniabut <eugeniabutkevich2022@gmail.com>
2023-07-05 14:26:24 +0200

    eu

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRoutes);
-app.get("/", (eq, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome User ");
 });
 app.use("/books", booksRoutes);
